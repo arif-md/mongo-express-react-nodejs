92347444790470d9c2d2ef0e68ab9dbe14cf8372
Arif Mohammed <arifmohammed.technical@gmail.com>
2025-01-27 07:44:09 -0600

    update to server.js to run both client and server on same port

--- server/server.js
+++ server/server.js
@@ -11,15 +11,19 @@ const appRoutes = require("./routes");
 //Setup server PORT
 const port = process.env.PORT || 5000;
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded());
 // parse requests of content-type - application/json
 app.use(express.json());
-const corsOptions = {
+/*const corsOptions = {
     origin: 'http://localhost:3000'
-}
-app.use(cors(corsOptions));
-
+}*/
+app.use(cors());
+app.use(express.static(path.join(__dirname,".","build")))
+app.use(express.static("public"));
 app.use("/api", appRoutes);
+app.use((req, res)=>{
+    res.sendFile(path.join(__dirname,".","build","index.html"))
+})
 
 // Handle invalid OR 404 request
 app.use((_, res) => {
